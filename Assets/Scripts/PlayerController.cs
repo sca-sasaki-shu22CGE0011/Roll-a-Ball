a1035af5728d7b7bcbbf6d5f56dd2f490842fc1d
PG22-01 <>
2022-05-28 13:44:49 +0900

    データの再アップロード

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20;
+    public Text scoreText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        score = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // カーソルキーの入力を取得
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        // カーソルキーの入力に合わせて移動方向を設定
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        // Ridigbody に力を与えて玉を動かす
+        rb.AddForce(movement * speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // ぶつかったオブジェクトが収集アイテムだった場合
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            // その収集アイテムを非表示にします
+            other.gameObject.SetActive(false);
+
+            score = score + 1;
+
+            SetCountText ();
+        }
+    }
+
+    void SetCountText()
+    {
+        scoreText.text = "Count: " + score.ToString();
+
+        if (score >= 3)
+        {
+            winText.text = "You Win";
+        }
+    }
+}
